76ea7841f544a490ce1c8cc01e0d18af27cc845c
史 翔新 <x_shi@yumemi.co.jp>
2019-03-21 22:24:00 +0900

    init project

---
+++ SteinsKit/SteinsKit/SteinsKit.h
@@ -0,0 +1,19 @@
+//
+//  SteinsKit.h
+//  SteinsKit
+//
+//  Created by 史 翔新 on 2019/03/21.
+//  Copyright © 2019 Crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SteinsKit.
+FOUNDATION_EXPORT double SteinsKitVersionNumber;
+
+//! Project version string for SteinsKit.
+FOUNDATION_EXPORT const unsigned char SteinsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SteinsKit/PublicHeader.h>
+
+
