21b8d18f6eeb11135a363d3fa34dcd8712bfe7e6
史 翔新 <x_shi@yumemi.co.jp>
2019-03-22 12:13:35 +0900

    A prototype implement: - Variable and MappedVariable for initial use and map use - Observation for observing data as well as memory management - ExecutionMethod for choosing queue for execution

---
+++ SteinsKit/SteinsKit/Observation.swift
@@ -0,0 +1,74 @@
+//
+//  Observation.swift
+//  SteinsKit
+//
+//  Created by 史 翔新 on 2019/03/21.
+//  Copyright © 2019 Crazism. All rights reserved.
+//
+
+import Foundation
+
+struct Observation<Value> {
+    
+    private(set) weak var observer: AnyObject?
+    private let method: ExecutionMethod
+    private var action: (Value) -> Void
+    
+    private let hashObject: NSObject = .init()
+    
+    init <Observer: AnyObject> (observer: Observer, method: ExecutionMethod, observingAction: @escaping (Observer, Value) -> Void) {
+        
+        let action: (Value) -> Void = { [weak observer] value in
+            if let observer = observer {
+                observingAction(observer, value)
+            }
+        }
+        
+        self.observer = observer
+        self.method = method
+        self.action = action
+        
+    }
+    
+}
+
+extension Observation {
+    
+    var observerDeinited: Bool {
+        return observer == nil
+    }
+    
+    func run (with value: Value) {
+        
+        func execute() { action(value) }
+        
+        switch method {
+        case .directly:
+            execute()
+            
+        case .syncOnQueue(let queue):
+            queue.sync { execute() }
+            
+        case .asyncOnQueue(let queue):
+            queue.async { execute() }
+        }
+        
+    }
+    
+}
+
+extension Observation: Equatable {
+    
+    static func == (lhs: Observation<Value>, rhs: Observation<Value>) -> Bool {
+        return lhs.hashObject === rhs.hashObject
+    }
+    
+}
+
+extension Observation: Hashable {
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(hashObject)
+    }
+    
+}
